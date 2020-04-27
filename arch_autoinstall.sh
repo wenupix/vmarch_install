d688795bf63654a22a9c65c6371561f6e97730e2
Jonatan Huenupil F <wenupix@gmail.com>
2020-04-26 22:02:50 -0400

    NEW: script

---
+++ arch_autoinstall.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+HDD_DST="/dev/sda"
+MNT_DST="/mnt"
+USR_DEF="wenupix"
+
+echo "==> Iniciando Configuracion de sistema Archlinux"
+# ---
+echo "==> Verificando disco $HDD_DST"
+# particiones
+HDD_NPART=$( fdisk -l $HDD_DST | grep ^$HDD_DST | wc -l )
+if [ $HDD_NPART -ne 0 ]; then
+    echo "!! AVISO: disco usado."
+    echo "  -> Limpiando"
+    dd if=/dev/zero of=$HDD_DST bs=8M count=64 oflag=sync status=progress && sync
+    #echo "!! Limipiar disco antes de continuar."
+    #exit 10
+fi
+
+# valores en MB
+HDD_BOOT_PARTSZ=512
+HDD_SWAP_PARTSZ=1024
+HDD_SIZEb=$( lsblk -blpn $HDD_DST | grep disk | awk '{ print $4 }' )
+HDD_SIZEM=$( echo $(( $HDD_SIZEb / 1024 / 1024 )) )
+HDD_ROOT_PARTSZ=$( echo $(( $HDD_SIZEM - $HDD_BOOT_PARTSZ - $HDD_SWAP_PARTSZ )) )
+
+echo "==> Particionando disco"
+parted -s -a optimal $HDD_DST unit MB mklabel msdos mkpart primary ext3 2 $HDD_BOOT_PARTSZ \
+set 1 boot on \
+mkpart primary ext4 $HDD_BOOT_PARTSZ $HDD_ROOT_PARTSZ \
+mkpart primary linux-swap $HDD_ROOT_PARTSZ 100%
+echo "==> Verificando tabla de particiones"
+parted $HDD_DST print
+
+echo "==> Activando SWAP"
+swapon $HDD_DST\3
+echo "==> Montando carpetas"
+mount $HDD_DST\2 $MNT_DST
+mkdir -p /mnt/boot
+mount $HDD_DST\1 $MNT_DST/boot
+echo "==> Instalando paquetes"
+pacstrap $MNT_DST base base-devel linux grub networkmanager wget git vim openssh
+echo "==> Post-configuracion"
+genfstab -Up $MNT_DST > $MNT_DST/etc/fstab
+echo "vm-archlinux" > $MNT_DST/etc/hostname
+echo "LANG=es_CL.UTF-8" > $MNT_DST/etc/locale.conf
+echo "KEYMAP=es" > $MNT_DST/etc/vconsole.conf
+arch-chroot $MNT_DST sed -i 's/#es_CL.UTF-8/es_CL.UTF-8/' /etc/locale.gen
+arch-chroot $MNT_DST locale-gen
+arch-chroot $MNT_DST grub-install $HDD_DST
+sync
+arch-chroot $MNT_DST mkinitcpio -p linux
+sync
+arch-chroot $MNT_DST grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot $MNT_DST echo "root:root" | chpasswd
+arch-chroot $MNT_DST useradd -m -g users -G wheel,network,power,audio,video,lp -s /bin/bash $USR_DEF
+arch-chroot $MNT_DST echo "$MNT_DST:$MNT_DST" | chpasswd
+#
+umount -R $MNT_DST
